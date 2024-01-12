c12af13a37f30962281bd47da67653ee054a73d7
Catcathg <catherine-guo@outlook.fr>
2024-01-12 14:35:21 +0100

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,13 @@
-import Header  from './Componenets/Header'
 import './App.css'
 import Nav from './Pages/Home/Nav'
+
 
 
 function App() {
 
   return (
     <>
-    <Header />
-      <Nav />
+      <Nav/>
     </>
   )
 }
