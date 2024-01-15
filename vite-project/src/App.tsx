af219091691b53f2b5d58fd9bea148f08a24b208
Catcathg <catherine-guo@outlook.fr>
2024-01-15 15:08:38 +0100

    router

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,24 @@
-import './App.css'
-import Home from './Pages/Home/Home'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+
+// pages
+import Home from './Pages/Home'
+import About from './Pages/About'
 
 
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/about/'} element={<About />} />
+        <Route path={'*'} element={<>Erreur 404</>} />
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
--- vite-project/src/Pages/Home.tsx
+++ vite-project/src/Pages/Home.tsx
@@ -1,9 +1,10 @@
-import Nav from '../../Componenets/Nav'
+import Nav from '../Componenets/Nav'
 
 export default function Home() {
   return (
     <>
     <Nav/>
+    <h1>Home</h1>
     </>
   )
 }
