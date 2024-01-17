239556459ed56a255e540730e45424e2df844a8d
Catcathg <catherine-guo@outlook.fr>
2024-01-17 15:13:56 +0100

    body left finish

--- vite-project/src/Components/Content.tsx
+++ vite-project/src/Components/Content.tsx
@@ -4,32 +4,37 @@ import Citroen from '@/assets/Images/citroen.svg'
 import Renault from '@/assets/Images/Renault.svg'
 import Skoda from '@/assets/Images/skoda.svg'
 import HistoricPurchase from '@/assets/Images/historicPurchase.png'
-
-
+import SearchBar from './searchBar'
 
 
 function Content() {
     return (
         <>
-            <div className="top">
-                <h1>Welcome back Baptiste !</h1>
-                <h2>Brands</h2>
-            </div>
-            <div className="bodyLeft">
-                <div className="brandLogo">
-                    <img src={Peugeot} />
-                    <img src={Citroen} />
-                    <img src={Renault} />
-                    <img src={Skoda} />
+            <div className="content">
+                <div className="top">
+                    <h1>Welcome back Baptiste !</h1>
+                    <h2>Brands</h2>
                 </div>
-                <p className="purchase">Purchase in progress</p>
-                <div className="historic">
-                    <img src={HistoricPurchase} />
+                <div className="bodyLeft">
+                    <div className="brandLogo">
+                        <img src={Peugeot} />
+                        <img src={Citroen} />
+                        <img src={Renault} />
+                        <img src={Skoda} />
+                    </div>
+                    <p className="purchase">Purchase in progress</p>
+                    <div className="historic">
+                        <img src={HistoricPurchase} />
+                    </div>
+                </div>
+                <div className="bodyCenter">
+                    <SearchBar />
                 </div>
             </div>
+
 
         </>
     )
 }
-    
+
 export default Content
