432c832fec24d984df40b67973cc412aca3dfeb6
Catcathg <catherine-guo@outlook.fr>
2024-01-17 13:33:59 +0100

    stickylist

--- vite-project/src/Components/Content.tsx
+++ vite-project/src/Components/Content.tsx
@@ -2,13 +2,14 @@ import background from '@/assets/Images/background.png'
 import legendBrand from '@/assets/Images/legendBrand.png'
 import legendStep from '@/assets/Images/legendStep.png'
 import ListBrand from '@/Components/listBrand';
+
 import './Content.css'
 
 
 function Content() {
     return (
         <>
-            <div>
+            <div className="image">
                 <img className="background" src={background} />
             </div>
             <div className="bodyLeft">
--- vite-project/src/Components/listBrand.tsx
+++ vite-project/src/Components/listBrand.tsx
@@ -3,33 +3,37 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
+import SearchBar from '@/Components/searchBar';
 
-export default function listBrand () {
+export default function listBrand() {
   return (
-    <List
-      sx={{
-        width: '100%',
-        maxWidth: 360,
-        bgcolor: 'background.paper',
-        position: 'relative',
-        overflow: 'auto',
-        maxHeight: 300,
-        '& ul': { padding: 0 },
-      }}
-      subheader={<li />}
-    >
-      {[0, 1, 2, 3, 4].map((sectionId) => (
-        <li key={`section-${sectionId}`}>
-          <ul>
-            <ListSubheader>{`Bonjour ${sectionId}`}</ListSubheader>
-            {[0, 1, 2].map((item) => (
-              <ListItem key={`item-${sectionId}-${item}`}>
-                <ListItemText primary={`Item ${item}`} />
-              </ListItem>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </List>
+    <div>
+    <SearchBar />
+      <List
+        sx={{
+          width: '100%',
+          maxWidth: 360,
+          bgcolor: 'background.paper',
+          position: 'relative',
+          overflow: 'auto',
+          maxHeight: 480,
+          '& ul': { padding: 0 },
+        }}
+        subheader={<li />}
+      >
+        {[0, 1, 2, 3, 4].map((sectionId) => (
+          <li key={`section-${sectionId}`}>
+            <ul>
+              <ListSubheader>{`Bonjour ${sectionId}`} </ListSubheader>
+              {[0, 1, 2].map((item) => (
+                <ListItem key={`item-${sectionId}-${item}`}>
+                  <ListItemText primary={`Item ${item}`} />
+                </ListItem>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </List>
+    </div>
   );
 }
