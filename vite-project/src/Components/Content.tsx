4c4ce64eacdc0d0cba41cc8ae7579d98f1592c0f
RaphaelPaul21 <148201361+RaphaelPaul21@users.noreply.github.com>
2024-01-17 15:44:02 +0100

    disposition div

--- vite-project/src/Components/Content.tsx
+++ vite-project/src/Components/Content.tsx
@@ -5,6 +5,7 @@ import Renault from '@/assets/Images/Renault.svg'
 import Skoda from '@/assets/Images/skoda.svg'
 import HistoricPurchase from '@/assets/Images/historicPurchase.png'
 import SearchBar from './searchBar'
+import ListIcons from './ListIcons'
 
 
 function Content() {
@@ -15,21 +16,25 @@ function Content() {
                     <h1>Welcome back Baptiste !</h1>
                     <h2>Brands</h2>
                 </div>
-                <div className="bodyLeft">
-                    <div className="brandLogo">
-                        <img src={Peugeot} />
-                        <img src={Citroen} />
-                        <img src={Renault} />
-                        <img src={Skoda} />
+                <div className="fullBody">
+                    <div className="bodyLeft">
+                        <div className="brandLogo">
+                            <img src={Peugeot} />
+                            <img src={Citroen} />
+                            <img src={Renault} />
+                            <img src={Skoda} />
+                        </div>
+                        <p className="purchase">Purchase in progress</p>
+                        <div className="historic">
+                            <img src={HistoricPurchase} />
+                        </div>
                     </div>
-                    <p className="purchase">Purchase in progress</p>
-                    <div className="historic">
-                        <img src={HistoricPurchase} />
-                    </div>
-                </div>
                 <div className="bodyCenter">
                     <SearchBar />
+                    <ListIcons />   
                 </div>
+                </div>
+            
             </div>
 
 
