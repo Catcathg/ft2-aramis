04d3da665593fd511d216872a34f176f6a5e4d38
Catcathg <catherine-guo@outlook.fr>
2024-01-19 10:09:57 +0100

    init

--- vite-project/src/Components/Content.tsx
+++ vite-project/src/Components/Content.tsx
@@ -7,6 +7,7 @@ import Car1 from '@/assets/Images/car1.png'
 import Car2 from '@/assets/Images/car2.png'
 import Car3 from '@/assets/Images/car3.png'
 import SearchBar from './searchBar'
+import List from './List'
 
 
 function Content() {
@@ -17,23 +18,41 @@ function Content() {
                     <h1>Welcome back Baptiste !</h1>
                     <h2>Brands</h2>
                 </div>
-                <div className="bodyLeft">
-                    <div className="brandLogo">
-                        <img src={Peugeot} />
-                        <img src={Citroen} />
-                        <img src={Renault} />
-                        <img src={Skoda} />
+                <div className="contentBody">
+                    <div className="bodyLeft">
+                        <div className="brandLogo">
+                            <img className="brand" src={Peugeot} />
+                            <img className="brand" src={Citroen} />
+                            <img className="brand"src  ={Renault} />
+                            <img className="brand" src ={Skoda} />
+                        </div>
+                        <p className="purchase">Purchase in progress</p>
+                        <div className="historic">
+                            
+                        </div>
                     </div>
-                    <p className="purchase">Purchase in progress</p>
-                    <div className="historic">
-                        <img src={HistoricPurchase} />
+                    <div className="bodyCenter">
+                        <SearchBar />
+                        <List />
+                    </div>
+                    <div className="bodyRight">
+                        <p className="stock">Lowest stocks</p>
+                        <div className="card">
+                            <div className="box1">
+                                <img src={Car1} />
+                                <span>Peugeot 108</span>
+                            </div>
+                            <div className="box2">
+                                <img src={Car2} />
+                                <span>Peugeot 508 SW</span>
+                            </div>
+                            <div className="box3">
+                                <img src={Car3} />
+                                <span>Jumper Fourgon</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="bodyCenter">
-                    <SearchBar />
-                </div>
-                </div>
-            
             </div>
 
 
